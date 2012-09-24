c19f6f065423f2b96b2acd8727aea1fde4833d34
leibreichb1@gmail.com <leibreichb1@gmail.com>
2012-09-24 17:30:30 -0400

    started working on the tracking map

--- src/org/research/thevault/maps/TrackMapActivity.java
+++ src/org/research/thevault/maps/TrackMapActivity.java
@@ -4,44 +4,94 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+import org.research.chatclient.CreateAccountActivity;
 import org.research.chatclient.R;
 
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.google.android.maps.MapActivity;
 
-public class TrackMapActivity extends MapActivity{
+public class TrackMapActivity extends MapActivity {
 
 	private ListView lv;
 	private ProgressDialog mProgress;
 	private JSONArray mUsers;
-	
+	private SharedPreferences mPrefs;
+
 	@Override
 	protected void onCreate(Bundle arg0) {
 		super.onCreate(arg0);
 		setContentView(R.layout.track_map);
 		lv = (ListView) findViewById(R.id.trackedList);
+		View listHeader = (View) getLayoutInflater().inflate(R.layout.list_header, null, false);
+		Button doneBtn = (Button) listHeader.findViewById(R.id.listHeaderBtn);
+		doneBtn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				String users = "";
+				try {
+					for(int i = 0; i < mUsers.length()-1; i++)
+						users += mUsers.get(i).toString().trim() + ",";
+					users += mUsers.get(mUsers.length()-1).toString().trim() + ",";
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				if(!users.equals("")){
+					HttpPost post = new HttpPost("http://devimiiphone1.nku.edu/research_chat_client/password_vault_server/get_locations.php");
+					try {
+						List<NameValuePair> nvp = new LinkedList<NameValuePair>();
+						nvp.add(new BasicNameValuePair("users", users));
+				        post.setEntity(new UrlEncodedFormEntity(nvp));
+					} catch (UnsupportedEncodingException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					new GetLocationsTask().execute(post);
+				}
+
+			}
+		});
+		lv.addHeaderView(listHeader);
 		mProgress = new ProgressDialog(this);
-	    mProgress.setIndeterminate(true);
-	    mProgress.setCancelable(false);
-	    mProgress.setMessage("Getting Tracked Users...");
-	    mProgress.show();
-		new GetPeopleTask().execute(new HttpGet("http://devimiiphone1.nku.edu/research_chat_client/chat_client_server/get_users.php"));
-		
+		mProgress.setIndeterminate(true);
+		mProgress.setCancelable(false);
+		mProgress.setMessage("Getting Tracked Users...");
+		mProgress.show();
+
+		mPrefs = getSharedPreferences(CreateAccountActivity.PREFS, Context.MODE_PRIVATE);
+
+		new GetPeopleTask().execute(new HttpGet("http://devimiiphone1.nku.edu/research_chat_client/password_vault_server/get_tracked_users.php"));
+
 	}
-	
+
 	@Override
 	protected boolean isRouteDisplayed() {
 		// TODO Auto-generated method stub
@@ -49,11 +99,12 @@ public class TrackMapActivity extends MapActivity{
 	}
 
 	private class GetPeopleTask extends AsyncTask<HttpGet, Void, InputStream> {
-	    @Override
-		 protected InputStream doInBackground(HttpGet... post) {
-	    	HttpClient httpclient = new DefaultHttpClient();
-	    	InputStream stream = null;
-	    	HttpResponse response;
+
+		@Override
+		protected InputStream doInBackground(HttpGet... post) {
+			HttpClient httpclient = new DefaultHttpClient();
+			InputStream stream = null;
+			HttpResponse response;
 			try {
 				response = httpclient.execute(post[0]);
 				HttpEntity entity = response.getEntity();
@@ -61,33 +112,34 @@ public class TrackMapActivity extends MapActivity{
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-	        return stream;
-	     }
+			return stream;
+		}
 
-	     @Override
-	     protected void onPostExecute(InputStream result) {
-	    	 String text = "";
-	    	 
-	    	 try{
-	    		 if( result != null ){
-			    	 BufferedReader br = new BufferedReader(new InputStreamReader(result));
-			    	 String line = br.readLine();
-			    	 
-			    	 while( line != null ){
-			    		 text += line + " ";
-			    		 line = br.readLine();
-			    	 }
-	    		 }
-	    	 }catch (IOException e) {
+		@Override
+		protected void onPostExecute(InputStream result) {
+			String text = "";
+
+			try {
+				if (result != null) {
+					BufferedReader br = new BufferedReader(new InputStreamReader(result));
+					String line = br.readLine();
+
+					while (line != null) {
+						text += line + " ";
+						line = br.readLine();
+					}
+				}
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
-	    	 
-	    	 if(mProgress.isShowing())
-	    		 mProgress.dismiss();
-	    	 try {
+
+			if (mProgress.isShowing())
+				mProgress.dismiss();
+			try {
+				Log.d("RES", "RES: " + text);
 				mUsers = new JSONArray(text);
 				String[] users = new String[mUsers.length()];
-				for(int i = 0; i < mUsers.length(); i++)
+				for (int i = 0; i < mUsers.length(); i++)
 					users[i] = mUsers.getString(i);
 				ArrayAdapter<String> adapter = new ArrayAdapter<String>(TrackMapActivity.this, android.R.layout.simple_list_item_1, users);
 				adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -95,6 +147,60 @@ public class TrackMapActivity extends MapActivity{
 			} catch (JSONException e) {
 				e.printStackTrace();
 			}
-	     }
-	 }
+		}
+	}
+
+	private class GetLocationsTask extends AsyncTask<HttpPost, Void, InputStream> {
+
+		@Override
+		protected InputStream doInBackground(HttpPost... post) {
+			HttpClient httpclient = new DefaultHttpClient();
+			InputStream stream = null;
+			HttpResponse response;
+			try {
+				response = httpclient.execute(post[0]);
+				HttpEntity entity = response.getEntity();
+				stream = entity.getContent();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return stream;
+		}
+
+		@Override
+		protected void onPostExecute(InputStream result) {
+			String text = "";
+
+			try {
+				if (result != null) {
+					BufferedReader br = new BufferedReader(new InputStreamReader(result));
+					String line = br.readLine();
+
+					while (line != null) {
+						text += line + " ";
+						line = br.readLine();
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			if (mProgress.isShowing())
+				mProgress.dismiss();
+			try {
+				Log.d("RES", "RES: " + text);
+				JSONObject json = new JSONObject(text);
+				JSONArray spen = json.getJSONArray("spencer");
+				Log.d("Spencer", spen.toString());
+//				String[] users = new String[mUsers.length()];
+//				for (int i = 0; i < mUsers.length(); i++)
+//					users[i] = mUsers.getString(i);
+//				ArrayAdapter<String> adapter = new ArrayAdapter<String>(TrackMapActivity.this, android.R.layout.simple_list_item_1, users);
+//				adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//				lv.setAdapter(adapter);
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
